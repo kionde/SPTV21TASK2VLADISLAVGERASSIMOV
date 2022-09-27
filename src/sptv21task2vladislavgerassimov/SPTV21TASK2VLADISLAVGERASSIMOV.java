b67c398f9025335b388e0351e2e48bcd84ec02f6
Vladislav Gerassimov <113329680+kionde@users.noreply.github.com>
2022-09-27 11:18:06 +0300

    Add files via upload

---
+++ src/sptv21task2vladislavgerassimov/SPTV21TASK2VLADISLAVGERASSIMOV.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptv21task2vladislavgerassimov;
+
+/**
+ *
+ * @author user
+ */
+public class SPTV21TASK2VLADISLAVGERASSIMOV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=859;
+        System.out.println("Сумма цифр числа " +a +" равна "+((a%10)+((a/10)%10)+((a/100)%10)));
+        // TODO code application logic here
+    }
+    
+}
